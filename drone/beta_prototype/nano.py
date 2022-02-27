658f5e7b5cea280373e5d43b24615689484c5177
Alexander Knapen <aknapen@calpoly.edu>
2022-02-27 15:22:56 -0800

    cleaned up script codes; added comments

--- drone/beta_prototype/nano.py
+++ drone/beta_prototype/nano.py
@@ -1,17 +1,19 @@
 import socket as sock
-import logging as logger
+import logging
 import threading as thread
 import cv2 as opencv
 import numpy as np
 import struct
 
+# IP address and port number for the Jetson Nano
+NANO_ADDR = ("172.16.0.2", 49152)
+
+# IP address and port number for the Raspberry Pi
+RPI_ADDR = ("172.16.0.1", 49152)
+
+# Maximum allowed data tranfser size over Ethernet
 MAX_DATA_SIZE = 2**16
 
-# addresses of the Jetson Nano and Raspberry Pi
-# in the form of (IP address, port number) pairs
-NANO_MSG_ADDR = ("172.16.0.2", 49152)
-NANO_VID_ADDR = ("172.16.0.2", 49153)
-RPI_MSG_ADDR = ("172.16.0.1", 49152)
 
 def dump_buffer(frame):
     while True:
@@ -25,12 +27,12 @@ def receive_video():
 
     # Bind the UDP video socket to the desired port and IP address
     try:
-        vidSock.bind(NANO_VID_ADDR)
-        logger.info("Bound receiver to video addr (IP: %s, Port: %d)\n", NANO_VID_ADDR[0], NANO_VID_ADDR[1])
+        vidSock.bind(NANO_ADDR)
+        logger.info("Bound receiver to video addr (IP: %s, Port: %d)\n", NANO_ADDR[0], NANO_ADDR[1])
         data, addr = vidSock.recvfrom(MAX_DATA_SIZE)
-        vidSock.sendto(b'Hello Nano', RPI_MSG_ADDR)
+        vidSock.sendto(b'Hello Nano', RPI_ADDR)
         data = b'' # data buffer to read into
-        #dump_buffer(vidSock)
+        dump_buffer(vidSock)
 
         # Define the video codec for MP4 files (codec = encoder and decoder for video compression)
         codec = opencv.VideoWriter_fourcc(*'mp4v')
@@ -41,9 +43,13 @@ def receive_video():
 
         while True:
             packet, addr = vidSock.recvfrom(MAX_DATA_SIZE)
+
+            # Check if the incoming packet is a signal from the RPi to stop listening for video feed
             if struct.unpack("!I", packet[0:4])[0] == 0xdeadbeef:
                 print("Received all done from sender...")
                 break
+
+            # Check if we've reached the last sequence number for the packets making up the current frame
             if struct.unpack("B", packet[0:1])[0] > 1:
                 data += packet[1:]
             else:
@@ -72,36 +78,9 @@ def receive_video():
         
 if __name__ == "__main__":
 
+    # Configure logging info for this application (show all debug info by default)
+    logging.basicConfig(format='%(asctime)s | %(levelname)s: %(message)s', level=logging.NOTSET)
+    nano_logger = logging.getLogger("nano")
+
+    # Begin running the application
     receive_video()
-'''
-    receiver = thread.Thread(target=receive_video)
-    receiver.start()
-
-    # Create a UDP socket object to handle the communication
-    udpSocket = sock.socket(family=sock.AF_INET, # using IP address family
-                            type=sock.SOCK_DGRAM, # using UDP protocol
-                            proto=0 # unused parameter for UDP
-                           )
-
-    # Bind the UDP socket to the desired port and IP address
-    try:
-        # Bind the socket to the Pi's IP address and port number
-        # Allows the Raspberry Pi's socket to receive data from the Jetson Nano
-        udpSocket.bind(NANO_MSG_ADDR)
-        logger.info("Bound to (IP: %s, Port: %d)\n", NANO_MSG_ADDR[0], NANO_MSG_ADDR[1])
-
-        data, addr = udpSocket.recvfrom(MAX_DATA_SIZE)
-        print("Received message from {}: {}\n".format(addr, data))
-        
-        # Message Loop
-        while True:
-            msg = input("> ")
-
-            udpSocket.sendto(msg, RPI_MSG_ADDR)
-
-            data, addr = udpSocket.recvfrom(MAX_DATA_SIZE)
-            print("Raspberry PI: {}".format(data))
-
-    except sock.error as msg:
-        logger.error("bind() call raised exception: %s\n", msg.strerror)
-'''
--- drone/beta_prototype/rpi.py
+++ drone/beta_prototype/rpi.py
@@ -11,29 +11,35 @@ from server import run_server
 
 MAX_DATA_SIZE = 2**16 # maximum payload size for UDP packet is 2^16 = 65536 bytes
 
-UDP_PORT = 49152 # use the first user-allocated port number for UDP communication
-UDP_IP = "172.16.0.1" # Ethernet IP address for the Raspberry Pi
-BUFFER_SIZE = 65536
+# IP address and port number for the Jetson Nano
+NANO_ADDR = ("172.16.0.2", 49152)
 
-NANO_MSG_PORT = 49152
-NANO_VID_PORT = 49153
-NANO_IP = "172.16.0.2"
-
-# Addresses of the Jetson Nano and Raspberry Pi
-# in the form of (IP address, port number) pairs
-NANO_MSG_ADDR = ("172.16.0.2", 49152)
-NANO_VID_ADDR = ("172.16.0.2", 49153)
-RPI_MSG_ADDR = ("172.16.0.1", 49152)
+# IP address and port number for the Raspberry Pi
+RPI_ADDR = ("172.16.0.1", 49152)
 
 # Global boolean to tell the code when to stop streaming video and clean up
 stop_stream = False
 
+
+'''
+    Signal handler that handles SIGINT being sent by the user to stop the RPI
+    application. The handler changes the Boolean value of stop_stream to True
+    so that the main loop in stream_video gets broken out of and the program
+    can exit.
+'''
 def signal_handler(signum, frame):
     global stop_stream 
 
     stop_stream = True # indicate to the program to exit the streaming loop
     print("\nVideo stopped at user request...")
 
+
+'''
+    Given a video frame, partitions the frame into a series of UDP packets
+    to be sent over the Ethernet connection. A sequence number is assigned 
+    to each packet of a given video frame to indicate to the receiver when
+    the last packet of a given video frame has been sent.
+'''
 def generate_udp_frames(data):
     payloadSize = len(data)
     num_packets = math.ceil(payloadSize / (MAX_DATA_SIZE - 64))
@@ -53,25 +59,35 @@ def generate_udp_frames(data):
     return packets
 
 
+'''
+    Video streaming thread of execution. Takes in video feed from the Raspberry
+    Pi's camera, splits it up into UDP frames, and sends those UDP frames over
+    Ethernet to the Jetson Nano for further processing.
+'''
 def stream_video():
+    # Set up the socket on which to communicate with the Jetson Nano
     vidSock = sock.socket(family=sock.AF_INET,
                           type=sock.SOCK_DGRAM,
                           proto=0)
-
     vidSock.bind(RPI_MSG_ADDR)
+
+    # Send an initial handshake packet to the Jetson Nano to establish connection
     hello_msg = bytearray("Hello", encoding="utf-8")
     vidSock.sendto(hello_msg, NANO_VID_ADDR)
-    data, addr = vidSock.recvfrom(MAX_DATA_SIZE)
-    #print("Received message {0} from ({1}, {2})\n".format(data, addr[0], addr[1]))
+    data, addr = vidSock.recvfrom(MAX_DATA_SIZE) # block here until acknowledgment received from Jetson Nano
+    
     # Open video file with data to send
-    #vid = opencv.VideoCapture("test.mp4") # video file for testing
     vid = opencv.VideoCapture(0) # capture frames from the Raspberry Pi camera
     while not stop_stream:
        status, frame = vid.read() # read a frame from the video file
        if not status:
            print("Video finished, exiting...")
            break
+
+       # Compress the video to reduce the bandwidth consumed by each transmitted video frame
        compressed = opencv.imencode('.jpg', frame)[1]
+
+       # Split the compressed video frame into a series of UDP packets to be transmitted
        udpFrames = generate_udp_frames(compressed.tostring())
        for i in range(0, len(udpFrames)): # send each frame serially to the Nano
             vidSock.sendto(udpFrames[i], NANO_VID_ADDR)
@@ -84,6 +100,7 @@ def stream_video():
     vid.release()
     opencv.destroyAllWindows()
     vidSock.close()
+
 
 if __name__ == "__main__":
 
@@ -98,5 +115,4 @@ if __name__ == "__main__":
     sender.start()
     server.start()
     
-    #server.join()
     sender.join()
