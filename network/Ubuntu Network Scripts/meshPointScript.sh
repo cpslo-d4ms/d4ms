8e3f83c8c3876baae80d6a9a15cf56873395d2ff
Joel Valdovinos Miranda <valdomjoel@gmail.com>
2022-05-05 23:15:33 -0700

    meshPointScript.sh now finds the name of the USB Wifi dongle instead of relying on a hard coded name for configuring the interface as a mesh point

--- network/Ubuntu Network Scripts/meshPointScript.sh
+++ network/Ubuntu Network Scripts/meshPointScript.sh
@@ -1,5 +1,8 @@
-sudo ifconfig wlx9cefd5fa5eef down
-sudo iw wlx9cefd5fa5eef set type mp
-sudo ifconfig wlx9cefd5fa5eef up
-sudo iw wlx9cefd5fa5eef mesh join PIMESH freq "$2" "$3"
-sudo ip addr add "$1"/24 dev wlx9cefd5fa5eef
+#!/bin/bash
+interfaceName=`ls /sys/class/net/ | grep wlx`
+
+sudo ifconfig $interfaceName down
+sudo iw $interfaceName set type mp
+sudo ifconfig $interfaceName up
+sudo iw $interfaceName mesh join PIMESH freq "$2" "$3"
+sudo ip addr add "$1"/24 dev $interfaceName
